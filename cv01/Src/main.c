72f7b671807fa8f7a579238a40d696c07c22e510
Frantisek Konecny <xkonec78@vutbr.cz>
2022-09-21 12:13:54 +0200

    cv1 - ad1 - LED

--- cv01/Src/main.c
+++ cv01/Src/main.c
@@ -18,12 +18,25 @@
 
 #include <stdint.h>
 
+#include "stm32f030x8.h"
+
+
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
 int main(void)
 {
+
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+	GPIOA->MODER |= GPIO_MODER_MODER5_0;
+
     /* Loop forever */
-	for(;;);
+	for(;;) {
+
+		GPIOA->ODR ^= (1<<5);
+		for (volatile uint32_t i = 0; i < 100000; i++) {}
+
+	}
 }
+
