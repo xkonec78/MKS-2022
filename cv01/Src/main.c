7d3df7dd258d6bb512cce55b62367b6b06bff16f
Frantisek Konecny <xkonec78@vutbr.cz>
2022-09-21 12:50:18 +0200

    cv1 - ad2 - SOS

--- cv01/Src/main.c
+++ cv01/Src/main.c
@@ -20,23 +20,33 @@
 
 #include "stm32f030x8.h"
 
-
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
-  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
+#warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
-int main(void)
-{
+int main(void) {
 
 	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
 	GPIOA->MODER |= GPIO_MODER_MODER5_0;
 
-    /* Loop forever */
-	for(;;) {
+	uint8_t code[32] = { 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1,
+			0, 0, 1, 0, 1, 0, 1 };
 
-		GPIOA->ODR ^= (1<<5);
-		for (volatile uint32_t i = 0; i < 100000; i++) {}
+	/* Loop forever */
+	for (;;) {
 
+		for (uint8_t ind = 0; ind < 32; ind++) {
+
+			if (code[31 - ind] == 1) {
+				GPIOA->BSRR = (1 << 5);
+			} else {
+				GPIOA->BRR = (1 << 5);
+			}
+		}
+
+		for (volatile uint32_t i = 0; i < 100000; i++) {
+		}
 	}
+
 }
 
